74875daaf7f9d6b558492dc0be76f16164b83517
Gease-RS <geasebrasil@gmail.com>
2019-01-16 00:01:53 -0200

    Conexao MongoDb MLab e Studio 3T

--- src/app.js
+++ src/app.js
@@ -1,8 +1,12 @@
 'use strict'
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
 const app = express();
 const router = express.Router();
+
+mongoose.connect('mongodb://admin:admin78@ds157654.mlab.com:57654/nodeapi');
 
 const indexRoute = require('./routes/index-route');
 const productRoute = require('./routes/product-route');
